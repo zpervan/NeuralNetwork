034f6f65d613540dcca6719267b6d4f7376dbacc
Zvonimir Pervan <z.pervan65@gmail.com>
2020-05-10 20:20:23 +0200

    Extend Neuron class with ActivationFunction result field

--- Libraries/Base/neuron.cpp
+++ Libraries/Base/neuron.cpp
@@ -5,3 +5,7 @@ double Neuron::GetValue() const { return value_; }
 Id Neuron::GetId() const { return id_; }
 
 void Neuron::SetValue(const double &value) { value_ = value; }
+
+double Neuron::GetResult() const { return result_; }
+
+void Neuron::SetResult(const double &result) { result_ = result; }
--- Libraries/Base/neuron.h
+++ Libraries/Base/neuron.h
@@ -2,23 +2,29 @@
 #define NEURALNETWORK_NEURON_H
 
 #include "neural_network_architecture_data.h"
+#include <utility>
+#include <vector>
 
-/// @brief Defines one neuron
+/// @brief Defines one neuron. Each neuron is aware of it's parent node.
 class Neuron {
 public:
-  explicit Neuron(const Id id, const double value) : id_(id), value_(value){};
+  Neuron(const Id id, const double value = 0, const double result = 0)
+      : id_(id), value_(value), result_(result){};
 
   Id GetId() const;
-  double GetValue() const;
+  Value GetValue() const;
+  double GetResult() const;
 
   void SetValue(const double &value);
+  void SetResult(const double &result);
 
   Neuron() = default;
   ~Neuron() = default;
 
 private:
   const Id id_;
-  double value_;
+  Value value_;
+  Value result_;
 };
 
 #endif // NEURALNETWORK_NEURON_H
