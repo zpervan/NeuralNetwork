24b667194537c707629b67f09bd3dd6b751b83d7
Zvonimir Pervan <z.pervan65@gmail.com>
2020-05-10 10:12:27 +0200

    Activation function bugfix and test extension

--- Libraries/Test/activation_functions_test.cpp
+++ Libraries/Test/activation_functions_test.cpp
@@ -4,12 +4,12 @@
 
 class ActivationFunctionTestFixture : public ::testing::Test {
 protected:
-  const std::array<const double, 3> inputs_{{0.5, 8.45, -0.21}};
+  const std::array<const double, 4> inputs_{{1.0, 0.5, 8.45, -0.21}};
 };
 
 TEST_F(ActivationFunctionTestFixture,
        GivenInput_WhenCalculatingLinearActivationFunction_ThenResultIsCorrect) {
-  const std::array<const double, 3> expected_value{0.5, 8.45, -0.21};
+  const std::array<const double, 4> expected_value{1.0, 0.5, 8.45, -0.21};
 
   for (std::size_t i = 1; i < expected_value.size(); i++) {
     const double actual_value = ActivationFunction::Linear(inputs_.at(i));
@@ -20,7 +20,8 @@ TEST_F(ActivationFunctionTestFixture,
 TEST_F(
     ActivationFunctionTestFixture,
     GivenInput_WhenCalculatingSigmoidActivationFunction_ThenResultIsCorrect) {
-  const std::array<const double, 3> expected_value{2.54149, 1.00021, -4.27939};
+  const std::array<const double, 4> expected_value{0.73105, 0.62245, 0.99978,
+                                                   0.44769};
 
   for (std::size_t i = 1; i < expected_value.size(); i++) {
     const double actual_value = ActivationFunction::Sigmoid(inputs_.at(i));
@@ -31,7 +32,8 @@ TEST_F(
 TEST_F(
     ActivationFunctionTestFixture,
     GivenInput_WhenCalculatingHyperbolicTangentActivationFunction_ThenResultIsCorrect) {
-  const std::array<const double, 3> expected_values{0.17000, 0.00000, -0.31499};
+  const std::array<const double, 4> expected_values{0.10307, 0.17000, 0.00000,
+                                                    -0.31499};
 
   for (std::size_t i = 1; i < expected_values.size(); i++) {
     const double actual_value =
--- Libraries/activation_functions.h
+++ Libraries/activation_functions.h
@@ -9,7 +9,7 @@ namespace ActivationFunction {
 
 static inline double Linear(const double x) { return x; }
 
-static inline double Sigmoid(const double x) { return 1 / (1 - std::exp(-x)); }
+static inline double Sigmoid(const double x) { return 1 / (1 + std::exp(-x)); }
 
 static inline double HyperbolicTangent(const double x) {
   return (1 - std::exp(-2 * x)) / (1 + std::exp(2 * x));
