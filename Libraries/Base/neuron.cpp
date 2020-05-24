a83dc04a5f6f652bbb98a1056d0ccf344128fc3b
Zvonimir Pervan <z.pervan65@gmail.com>
2020-05-24 18:34:47 +0200

    Add output target data member. Minor refactor.

--- Libraries/Base/neuron.cpp
+++ Libraries/Base/neuron.cpp
@@ -1,11 +1,17 @@
 #include "neuron.h"
 
-double Neuron::GetValue() const { return value_; }
-
 Id Neuron::GetId() const { return id_; }
 
-void Neuron::SetValue(const double& value) { value_ = value; }
+Value Neuron::GetValue() const { return value_; }
 
-double Neuron::GetActivationFunctionResult() const { return activation_func_result_; }
+Value Neuron::GetActivationFunctionResult() const { return activation_func_result_; }
 
-void Neuron::SetActivationFunctionResult(const double& result) { activation_func_result_ = result; }
+Value Neuron::GetOutputTarget() const { return output_target_; }
+
+void Neuron::SetValue(Value value) { value_ = value; }
+
+void Neuron::SetActivationFunctionResult(
+        Value activation_func_result) { activation_func_result_ = activation_func_result; }
+
+void Neuron::SetOutputTarget(
+        const Value output_target) { output_target_ = output_target; }
--- Libraries/Base/neuron.h
+++ Libraries/Base/neuron.h
@@ -4,27 +4,31 @@
 #include "neural_network_architecture_data.h"
 #include <utility>
 #include <vector>
-
+/// @todo: consider making it a struct
 /// @brief Defines one neuron. Each neuron is aware of it's parent node.
 class Neuron {
 public:
-    Neuron(const Id id, const double value = 0, const double activation_func_result = 0)
-            :id_(id), value_(value), activation_func_result_(activation_func_result) { };
+    Neuron(const Id id, const Value value = 0,
+            const Value activation_func_result = 0,
+            const Value output_target = 0)
+            :id_(id), value_(value),
+             activation_func_result_(activation_func_result),
+             output_target_(output_target) { };
 
     Id GetId() const;
     Value GetValue() const;
-    double GetActivationFunctionResult() const;
+    Value GetActivationFunctionResult() const;
+    Value GetOutputTarget() const;
 
-    void SetValue(const double& value);
-    void SetActivationFunctionResult(const double& activation_func_result);
-
-    Neuron() = default;
-    ~Neuron() = default;
+    void SetValue(Value value);
+    void SetActivationFunctionResult(Value activation_func_result);
+    void SetOutputTarget(Value output_target);
 
 private:
-    const Id id_;
-    Value value_;
-    Value activation_func_result_;
+    const Id id_{0};
+    Value value_{0.0};
+    Value activation_func_result_{0.0};
+    Value output_target_{0.0};
 };
 
 #endif // NEURALNETWORK_NEURON_H
